49c9493992e6f7c5ebf2622b90c22a934276265f
OrokanaKiti <55463752+OrokanaKiti@users.noreply.github.com>
2019-10-24 16:31:31 -0400

    everything is ready to be written, filler text and other basic input initialized just need to implement the two methods and createa call and store to the duckduckgo site api system

---
+++ test_president.py
@@ -0,0 +1,42 @@
+#author- Sammie~ ♫
+#This test is designed to take the results from a DuckDuckGo Search on "presidents of the united states" (instant answers) and return it then process it with known information to ensure accuracy
+#this is lesson 10's HW project
+
+# ----------------------------------------------------------------------
+# import section
+# ----------------------------------------------------------------------
+
+import pytest
+
+# ----------------------------------------------------------------------
+# var section of the code
+# ----------------------------------------------------------------------
+
+#this is a list of known good inputs to test agianst
+presProper = ["Washington", "Adams", "Jefferson", "Madison", "Monroe", "Adams", "Jackson", "Buren", "Harrison", "Tyler", "Polk", "Taylor", "Fillmore", "Pierce", "Buchanan", "Lincoln",
+                  "Johnson", "Grant" , "Hayes", "Garfield" , "Arthur", "Cleveland" , "Harrison", "Cleveland" , "McKinley", "Roosevelt" , "Taft", "Wilson" , "Harding", "Coolidge" , "Hoover",
+                  "Roosevelt" , "Truman", "Eisenhower" , "Kennedy", "Johnson" , "Nixon", "Ford" , "Carter", "Reagan" , "Bush", "Clinton" , "Bush", "Obama" , "Trump"]
+#sample inputs used in inital testing or editing in the code
+sample_input_good = ["Adams"]
+sample_input_bad = ["JoeMama"]
+
+#the input from the api to be tested agianst known good inputs
+#duckDuckInput = []
+
+
+# ----------------------------------------------------------------------
+# testers
+# ----------------------------------------------------------------------
+
+def duckSearch():
+    #call for the duck search of presidents and place only last names in duckDuckInput[]
+    return "sampleText"
+
+def test_duckSearch():
+    #test the results of duckDuckInput[] for inconsistencies with presproper
+    assert 1 == 1 #for duckDuckInput[i] is in presProper[], i++,
+
+
+
+# end of program confirmation test
+print("program was allowed to fully execute")
