c848f15a24227feeca66eb1d0e3022bbaaee207c
OrokanaKiti <55463752+OrokanaKiti@users.noreply.github.com>
2019-10-24 21:27:31 -0400

    Just adding notes for reference later

--- test_president.py
+++ test_president.py
@@ -3,7 +3,8 @@
 #this is lesson 10's HW project
 # NOTE Finding a Instant search displaying all the presidents is taking up lots of time
 # NOTE "The program for the Lesson 10 lab on Integration tests and the REST api has been changed.  It now involves verifying Presidents of the US being listed on the White House website." Check this! We might not need DDG
-
+# NOTE for self later http://www.gregreda.com/2015/02/15/web-scraping-finding-the-api/ Might be useful to webscrape the data from a simple site
+# NOTE Attempting to webscrape from this site wish me luck https://www.presidentsusa.net/presvplist.html also a git hub with a similar concept https://github.com/hitch17/sample-data/blob/master/presidents.json
 
 # ----------------------------------------------------------------------
 # import section
@@ -12,6 +13,8 @@
 import pytest
 import requests
 import json
+#import BigSmiles
+#import WholesomeStuff!!!!
 
 # ----------------------------------------------------------------------
 # var section of the code
@@ -24,7 +27,7 @@ presProper = ["Washington", "Adams", "Jefferson", "Madison", "Monroe", "Adams", 
 #sample inputs used in inital testing or editing in the code
 sample_input_good = ["Adams"]
 sample_input_bad = ["JoeMama"]
-response = requests.get("https://www.whitehouse.gov/about-the-white-house/presidents/")
+response = requests.get("https://www.presidentsusa.net/presvplist.html?json")
 
 #the input from the api to be tested agianst known good inputs
 #duckDuckInput = []
@@ -38,9 +41,10 @@ def duckSearch():
     #call for the duck search of presidents and place only last names in duckDuckInput[]
     return "sampleText"
 
-def test_duckSearch():
-    #test the results of duckDuckInput[] for inconsistencies with presproper
-    assert 1 == 1 #for duckDuckInput[i] is in presProper[], i++,
+#PYTEST_TO_BE Commented out for ease of access
+#def test_duckSearch():
+#    #test the results of duckDuckInput[] for inconsistencies with presproper
+#    assert 1 == 1 #for duckDuckInput[i] is in presProper[], i++,
 
 def jprint(obj):
     #testing code to learn how to parse a JSON input from a API system
@@ -52,7 +56,7 @@ def jprint(obj):
 # ----------------------------------------------------------------------
 
 print(response.status_code)
-#print(response.json())
+print(response.json())
 #jprint(response.json())
 
 # end of program confirmation test
