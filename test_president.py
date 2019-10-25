365451727df41405ec5de22ebfc35d80697011e6
OrokanaKiti <55463752+OrokanaKiti@users.noreply.github.com>
2019-10-24 22:19:54 -0400

    teacher gave advices wishing for the bestiest <3 <3 <3 ♫

--- test_president.py
+++ test_president.py
@@ -5,6 +5,10 @@
 # NOTE "The program for the Lesson 10 lab on Integration tests and the REST api has been changed.  It now involves verifying Presidents of the US being listed on the White House website." Check this! We might not need DDG
 # NOTE for self later http://www.gregreda.com/2015/02/15/web-scraping-finding-the-api/ Might be useful to webscrape the data from a simple site
 # NOTE Attempting to webscrape from this site wish me luck https://www.presidentsusa.net/presvplist.html also a git hub with a similar concept https://github.com/hitch17/sample-data/blob/master/presidents.json
+# NOTE JSON FOUND THAT IS USABLE https://github.com/hitch17/sample-data/blob/master/presidents.json HAS A JSON FILE (INCORRECT DATA)
+# LASTMINUTE time is getting short- too little time to continue to follow a pipe dream.. i NEED a json file to work with
+# LASTMIUTE creating a Json File with all the data i needm, importing it as a var and then using IT to push my tests
+#NOTE according to teacher it is still positive to find the  https://api.duckduckgo.com/?q=Presidents+of+the+United+States&format=json&pretty=1 like i intiialy tried with no result.. trying teachers advice  ; w ; im so short on time
 
 # ----------------------------------------------------------------------
 # import section
@@ -19,6 +23,7 @@ import json
 # ----------------------------------------------------------------------
 # var section of the code
 # ----------------------------------------------------------------------
+
 
 #this is a list of known good inputs to test agianst
 presProper = ["Washington", "Adams", "Jefferson", "Madison", "Monroe", "Adams", "Jackson", "Buren", "Harrison", "Tyler", "Polk", "Taylor", "Fillmore", "Pierce", "Buchanan", "Lincoln",
@@ -69,3 +74,14 @@ print("program was allowed to fully execute")
 #def test_duckSearch_fail():
 #
 #   assert 1 == 12 #tester code
+
+#
+## File attmempt to scan data gathered from a site   *****CANCELLED DUE TO TEACHER INTERVENTION********
+#with open('data.txt') as json_file:
+#    data = json.load(json_file)
+ #   for p in data['people']:
+ #       print('Name: ' + p['name'])
+#
+#
+#
+#
